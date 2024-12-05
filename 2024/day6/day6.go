32088b42123788f18a40fd63176684f4849dd4e1
Leon <leon@embertechnology.nz>
2024-12-06 06:25:37 +1300

    DAMMIT

--- 2024/day6/day6.go
+++ 2024/day6/day6.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -16,12 +17,53 @@ func Solve() {
 	fmt.Println("Unique pos visited:", uniquePositions)
 }
 
-
+// Format: [y, x]
+var directions [][]int = [][]int{
+	{1, 0}, // Up
+	{0, 1}, //Right
+	{-1, 0}, //Down
+	{0, -1}, //Left
+}
 func guardRunaround(x, y int, grid [][]string) int {
+	uniquePositionsVisited := 0
+	// Start out going upwards
+	dirIndex := 0
+	direction := directions[dirIndex]
 
-	return -1
+	// Declare grid height and width
+	height, width := len(grid), len(grid[0])
+
+	// Map y positions to list of x positions (I am lazy)
+	visitedPositions := make( map[int][]int)
+
+	for x > 0 && y > 0 && x < width && y < height {
+		nextY, nextX := (y + direction[0]), (x + direction[1])
+		if grid[nextY][nextX] == "#" {
+			fmt.Println("Found turn point", nextY, nextX)
+			newDirIndex := (dirIndex + 1) % len(directions)
+			dirIndex = newDirIndex
+			direction = directions[newDirIndex]
+
+		}
+		if !visited(y, x, visitedPositions) {
+			uniquePositionsVisited += 1
+		}
+		visitedPositions[y] = append(visitedPositions[y], x)
+		y += direction[0]
+		x += direction[1]
+	}
+	fmt.Println("Exited at y:", y, ", x:", x)
+	return uniquePositionsVisited
 }
 
+
+func visited (y, x int, visited map[int][]int) bool {
+	fmt.Println("Checking y:", y, "x:", x, visited[y], "contains x:", slices.Contains(visited[y], x))
+	if !slices.Contains(visited[y], x) {
+		return false
+	}
+	return true
+}
 
 
 func getGuardPos (grid [][]string) (int, int) {
