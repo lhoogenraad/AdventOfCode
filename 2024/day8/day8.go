77f9d2015abef09ad5d7b67bc316dc42e3bee5f9
Leon <leon@embertechnology.nz>
2024-12-08 20:35:28 +1300

    cuck periodt

--- 2024/day8/day8.go
+++ 2024/day8/day8.go
@@ -14,7 +14,30 @@ type point struct {
 }
 
 func Solve () {
-	fmt.Println(readData())
+	land := readData()
+	var antennaPointsMap map[string][]point = make(map[string][]point)
+	// Populate points map
+	for y, row := range land {
+		for x, val := range row {
+			if val == "." {continue}
+			p := point{x: x, y: y}
+			antennaPointsMap[val] = append(antennaPointsMap[val], p)
+		}
+	}
+
+	_ = fillAntinodes(antennaPointsMap)
+}
+
+
+func fillAntinodes (points map[string][]point ) [][]string {
+	// Lazily grab 2d array of map
+	antinodes := readData()
+
+	for key, p := range points {
+		fmt.Println(key, "\t", p)
+	}
+
+	return antinodes
 }
 
 
